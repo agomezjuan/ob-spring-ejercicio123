d9635916e0237e066aabdc2eb2638b4abccbffac
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-01-01 23:33:13 +0100

    first commit

---
+++ src/test/java/com/ob/spring/ejercicio123/Ejercicio123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ob.spring.ejercicio123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
